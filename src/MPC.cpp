17e4a94a0d4018cf8d3fe9c8fc1123da108d370e
Mohamed EL Sobky <31813463+melsobky@users.noreply.github.com>
2018-05-20 23:56:22 +0200

    updated delay handling

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -121,12 +121,6 @@ class FG_eval {
       AD<double> delta0 = vars[delta_start + t - 1];
       AD<double> a0 = vars[a_start + t - 1];
 
-      // delay compensation
-      if (t > 1) {
-        a0 = vars[a_start + t - 2];
-        delta0 = vars[delta_start + t - 2];
-      }
-
       AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * x0 * x0 + coeffs[3]* x0 * x0 * x0;
       AD<double> psides0 = CppAD::atan(3*coeffs[3]*x0*x0 + 2*coeffs[2]*x0 + coeffs[1]);
 
